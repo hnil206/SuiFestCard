0f66663025ec2efafa630504f79a557dbb29430d
qhuy22601 <qhuy22601@gmail.com>
2025-08-27 08:51:22 +0700

    feat: header footer

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-black text-white">
+      <nav className="flex items-center justify-center px-5 py-4 lg:px-28 lg:py-7" aria-label="Main">
+        <img src="/logo.png" alt="logo Suifest" className="h-7 lg:h-10" />
+      </nav>
+    </header>
+  );
+}
