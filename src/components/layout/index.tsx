36afb2bb75f064f59c57d7949894704efddd208e
qhuy22601 <qhuy22601@gmail.com>
2025-08-27 11:56:59 +0700

    feat: resolve conflict

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-black">
       <Header />
       <main className="container mx-auto flex-grow p-4">{children}</main>
       <Footer />
